fb7cf08efaeab6e67134ed3ce31353c522aa9b06
sakhawat71 <sakhawat.showrav@gmail.com>
2024-04-01 16:42:49 +0600

    error page complete

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -2,12 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Dashboard from "../Layout/Dashboard";
 import Home from "../pages/Home/Home";
+import ErrorPage from "../components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <MainLayout></MainLayout>,
-        errorElement: <div></div>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
+import errorImg from "../../assets/error.jpg"
+import { FaArrowLeft } from "react-icons/fa";
 
 const ErrorPage = () => {
     return (
-        <div>
-            <p>error</p>
+        <div className="">
+            <div>
+                <Link to="/" className="btn btn-outline hover:text-[#007DFE] hover:bg-white "><FaArrowLeft/> Home</Link>
+            </div>
+
+            <figure >
+                <img
+                    src={errorImg}
+                    alt="error page image"
+                    className="w-4/6 md:w-1/2 lg:w-1/3 mx-auto" />
+            </figure>
         </div>
     );
 };
