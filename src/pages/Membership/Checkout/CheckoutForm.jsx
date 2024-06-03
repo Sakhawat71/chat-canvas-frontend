fc84b134f42486aa2bcd19a54fa22289fe7a7abf
sakhawat71 <sakhawat.showrav@gmail.com>
2024-06-03 19:21:48 +0600

    design checkout form

--- src/pages/Membership/Checkout/CheckoutForm.jsx
+++ src/pages/Membership/Checkout/CheckoutForm.jsx
@@ -11,53 +11,33 @@ const CheckoutForm = () => {
         event.preventDefault();
 
         if (!stripe || !elements) {
-            // Stripe.js has not loaded yet. Make sure to disable
-            // form submission until Stripe.js has loaded.
             return;
         }
 
-        // Get a reference to a mounted CardElement. Elements knows how
-        // to find your CardElement because there can only ever be one of
-        // each type of element.
         const card = elements.getElement(CardElement);
 
         if (card == null) {
             return;
         }
 
-        // Use your card Element with other Stripe.js APIs
         const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: 'card',
             card,
         });
 
         if (error) {
-            console.log('[error]', error);
+            console.error('Error creating payment method:', error);
         } else {
-            console.log('[PaymentMethod]', paymentMethod);
+            console.log('Payment method created successfully:', paymentMethod);
         }
     };
 
     return (
         <form
             onSubmit={handleSubmit}
-            className="border-4 w-1/2 mt-10 flex-row items-center"
+            className="w-full md:w-2/3 lg:w-1/2 mt-10 flex-row items-center"
         >
             <CardElement
-                // options={{
-                //     style: {
-                //         base: {
-                //             fontSize: '16px',
-                //             color: '#424770',
-                //             '::placeholder': {
-                //                 color: '#aab7c4',
-                //             },
-                //         },
-                //         invalid: {
-                //             color: '#9e2146',
-                //         },
-                //     },
-                // }}
                 options={{
                     style: {
                         base: {
--- src/pages/Membership/Membership.jsx
+++ src/pages/Membership/Membership.jsx
@@ -2,17 +2,23 @@ import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import CheckoutForm from "./Checkout/CheckoutForm";
 
-const stripePromise = loadStripe('pk_test_51OwKrqG32hbyrQzeel69euEvH8bGduAvNGDykUiKC7WBL78vLyguRI0Dspg5PBxt6llwJUbMdKcY3L0Nqj9G8o2F00HRdTqTjg')
+const stripePromise = loadStripe(import.meta.env.VITE_Publishable_key)
 
 const Membership = () => {
 
+    // console.log(stripePromise);
 
     return (
         <div>
-            <Elements stripe={stripePromise}>
-                <CheckoutForm />
-            </Elements>
+
+
+            <div className="flex justify-center border-4 m-10 ">
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm />
+                </Elements>
+            </div>
         </div>
+
     );
 };
 
